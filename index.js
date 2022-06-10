f91c102d38fce728850b179f542a1c892ffa56b2
Dilara Katuk <dilarakatuk@gmail.com>
2022-06-10 18:52:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,23 +1,82 @@
 /* Your Code Here */
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+function createEmployeeRecord(arr)
+{
+return {
+    firstName: arr[0],
+    familyName: arr[1],
+    title: arr[2],
+    payPerHour: arr[3],
+    timeInEvents:[],
+    timeOutEvents:[],
+}
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+}
+const createEmployeeRecords = function(ArrayOfArrays) {
+    return ArrayOfArrays.map(function(array){
+        return createEmployeeRecord(array)
+    })
+}
+function createTimeInEvent(Datastamp)
+{
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(Datastamp.slice(-4)),
+        date: Datastamp.slice(0, 10)
+})
+return this
+}
+function createTimeOutEvent(Datastamp)
+{
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(Datastamp.slice(-4)),
+        date: Datastamp.slice(0, 10)
+})
+return this
+}
+function hoursWorkedOnDate(DataForm)
+{
+    let InTime = this.timeInEvents.find(function(e){
+        return e.date === DataForm
+    })
 
+    let OutTime = this.timeOutEvents.find(function(e){
+        return e.date === DataForm
+    })
+
+    return (OutTime.hour - InTime.hour) / 100
+}
+
+function wagesEarnedOnDate(DataForm)
+{
+    const Payowed = hoursWorkedOnDate(this, DataForm)* this.payPerHour
+    return parseFloat(Payowed.toString())
+}
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
-
     const payable = eligibleDates.reduce(function (memo, d) {
         return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+    }.bind(this), 0)
 
     return payable
 }
+function calculatePayroll (arrayofObject)
+{
+    let sum=0;
+    for (let i=0;i<arrayofObject.length;i++) {
+        sum += allWagesFor.call(arrayofObject[i]);
+      }
+    return sum
+}
+
+function findEmployeeByFirstName(Array, firstName) {
+    return Array.find((Person) => Person.firstName === firstName )
+  }
+
+  function wagesEarnedOnDate(date = "all") {
+    return this.payPerHour * hoursWorkedOnDate.call(this, date);
+  }
 
